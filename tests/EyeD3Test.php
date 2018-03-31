1afd95ad800576e720d941c5c495133b7188828b
Anas Mazouni <madadj4@gmail.com>
2018-03-31 23:18:43 +0100

    Added docs + album art support

--- tests/EyeD3Test.php
+++ tests/EyeD3Test.php
@@ -1,13 +1,15 @@
 <?php
 
 /**
-*  Corresponding Class to test EyeD3 class
+* eyeD3 wrapper test class | src/EyeD3.php
 *
-*  For each class in your library, there should be a corresponding Unit-Test for it
-*  Unit-Tests should be as much as possible independent from other test going on.
-*
-*  @author yourname
+* @author Stormiix <hello@stormix.co>
+* @package PSR\Stormiix\EyeD3
+* @license MIT
+* @version 1.0.0
+* @copyright Copyright (c) 2018, Stormix.co
 */
+
 class EyeD3Test extends \PHPUnit\Framework\TestCase
 {
 
@@ -20,22 +22,31 @@ class EyeD3Test extends \PHPUnit\Framework\TestCase
   */
     public function testIsThereAnySyntaxError()
     {
-        $var = new Stormiix\EyeD3\EyeD3;
+        $var = new Stormiix\EyeD3\EyeD3("assets/test.mp3");
         $this->assertTrue(is_object($var));
         unset($var);
     }
 
     /**
-    * Just check if the EyeD3 has no syntax error
-    *
-    * This is just a simple check to make sure your library has no syntax error. This helps you troubleshoot
-    * any typo before you even use this library in a real project.
-    *
+    * readMeta METHOD test
     */
-    public function testMethod1()
+    public function testreadMeta()
     {
-        $var = new Stormiix\EyeD3\EyeD3;
-        $this->assertTrue($var->method1("hey") == 'Hello World');
+        $var = new Stormiix\EyeD3\EyeD3("tests/assets/test.mp3");
+        $expected = "TestArtist";
+        $this->assertTrue($var->readMeta()["artist"] == $expected);
+        unset($var);
+    }
+
+    /**
+     * updateMeta METHOD test
+    */
+    public function testupdateMeta()
+    {
+        $var = new Stormiix\EyeD3\EyeD3("tests/assets/test.mp3");
+        $expected = "TestArtistChanged";
+        $var->updateMeta(["artist"=>$expected]);
+        $this->assertTrue($var->readMeta()["artist"] == $expected);
         unset($var);
     }
 }
